0c2b7febbc8ddaafb39da49c93e6696f9fcc4159
YanaTanasova <domenika.12@bigmir.net>
2017-07-14 13:17:19 +0300

    ContactUs дополнения

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function hiddenfild1(){
+function hiddenfield1(){
 	var n = $("#name").val()
 	
 	if (n.charAt()==0){
@@ -7,13 +7,13 @@ function hiddenfild1(){
 	
 	
 }
-function hiddenfild2(){
+function hiddenfield2(){
 var e = $("#e-mail").val()
 if (e.charAt()==0){
 		$(".he").css("opacity","1")
 	}
 }
-function hiddenfild3(){
+function hiddenfield3(){
 	var m = $("#message").val()
 	if (m.charAt()==0){
 		$(".hm").css("opacity","1")
